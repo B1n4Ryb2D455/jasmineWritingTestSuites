3ba82fc3d325ade32cab96e3ea970e3e35530e80
Sara Blevins <dvlnthebludrs@gmail.com>
2018-07-08 15:52:55 -0400

    Add add and delete contact functions

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,10 +1,20 @@
-describe('AddressBook', function() {
+describe('Address Book', function() {
     it('should be able to add a contact', function() {
-        var AddressBook = new AddressBook(),
+        var addressBook = new AddressBook(),
             thisContact = new Contact();
 
-            AddressBook.addContact(thisContact);
+            addressBook.addContact(thisContact);
 
-            expect(AddressBook.getContact(0)).toBe(thisContact);
+            expect(addressBook.getContact(0)).toBe(thisContact);
+    });
+
+    it('should be able to delete a contact', function() {
+        var addressBook = new AddressBook(),
+            thisContact = new Contact();
+
+        addressBook.addContact(thisContact);
+        addressBook.deleteContact(0);
+
+        expect(addressBook.getContact(0)).not.toBeDefined();
     });
 });
